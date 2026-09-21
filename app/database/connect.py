ef836c4d834e0418eba92a33b8c101cba6ca70e4
snihir <snihirdima10@gmail.com>
2026-09-21 23:43:27 +0200

    docker add

--- app/database/connect.py
+++ app/database/connect.py
@@ -2,7 +2,8 @@ import sqlite3
 from pathlib import Path
 
 def get_connection():
-    DB_PATH = Path(__file__).resolve().parent.parent.parent / "db.db"
+    BASE_DIR = Path(__file__).resolve().parents[2]
+    DB_PATH = BASE_DIR / "data" / "db.db"
     connection = sqlite3.connect(DB_PATH)
 
     connection.execute("PRAGMA foreign_keys = ON")
